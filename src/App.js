94e77bdcc0b79d34b04f0ba4bad2a2612281d929
Sam O'Brien-Olinger <samtobo@yahoo.co.uk>
2023-12-10 10:17:40 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Go Get Global
         </p>
         <a
           className="App-link"
